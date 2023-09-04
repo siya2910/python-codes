bfceeae8468a78ccf6c67556a4835afbd12601e3
siya2910 <143694322+siya2910@users.noreply.github.com>
2023-09-04 23:15:47 +0530

    4.py

---
+++ main.py
@@ -0,0 +1,16 @@
+print('hello wale')
+print('helping others is not that easy')
+print('''we all see many things around us like sometimes someone seek for help but we just ignore\n"
+       'nOT BECAUSE we all dont have heart its  because we dont consider us to that place 
+       'sometime keep yourself to that place then u will realise what they r going through \n'
+       'now coming to people like us who wants to help but they cant
+       'let me tell you how?''')
+print('''living in this world full of misery we r also having a life dealing with so many stuffs but not having money 
+ in this generation is another type of pain .like if a person needs any physical help then okay 
+ if they seek for mental health then also ok but if it is about money we middle class also have to surpass the situation
+ which is hated the most''')
+print('''like telling you all a small incident i saw one old man who is shivering but still selling fruits by cutting it
+then there they need economic help .At this point of time i cant help myself with money so how i could i help them''')
+print('''but i also see many peoples who can do works for their survival but they are not doing this and i salute who 
+work so hard for thier family and a good life ''')
+print('''so ending this on the last note whenever u found someone who needs help plz go for it''')
