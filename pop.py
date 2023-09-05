87ab91c2ede57d504d5d3a8b202ef5222343553e
siya2910 <143694322+siya2910@users.noreply.github.com>
2023-09-05 20:30:43 +0530

    6.PY

---
+++ pop.py
@@ -0,0 +1,11 @@
+print('Today topic is population')
+print('let me tell u why our country india has low literacy rate and economically low?')
+print('''because people dont understand the birth and death ratio so enormously birth rate is increasing day by day 
+      'more than death rate and this tells about the literacy rate of our country .people are not that aware of the 
+      terms of population balance status.''')
+print('''And these all birth rate effects directly our economic base because people are not earning that much too
+at least get a proper meal of three times then how they would be able to educate their child .There in our country 
+survival is big issue so education and literacy rate are not even in priority list.''')
+print('''so this all can be managed only when people get at least knowledge about sex education i.e.condoms ,operations,
+      'and many more.''')
+
